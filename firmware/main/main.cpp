69fd6f060a8d4c5d5af3a7762c9535defaa23523
cpm <cpm.cyril@gmail.com>
2026-08-21 01:12:28 +0200

    test

--- firmware/main/main.cpp
+++ firmware/main/main.cpp
@@ -4,36 +4,21 @@
 #include "CustomType.hpp"
 #include "driver/gpio.h"
 #include "Led.h"
+#include "HP_ESP32Servo.h"
 
-#define R_LED GPIO_NUM_12
-#define B_LED GPIO_NUM_27
-#define Y_LED GPIO_NUM_25
-#define G_LED GPIO_NUM_32
+Settingator& STR = Settingator::GetInstance();
 
-#define R_BUTTON GPIO_NUM_13
-#define B_BUTTON GPIO_NUM_14
-#define Y_BUTTON GPIO_NUM_26
-#define G_BUTTON GPIO_NUM_33
+#define PWM_0_PIN GPIO_NUM_27
+#define PWM_1_PIN GPIO_NUM_14
+#define PWM_2_PIN GPIO_NUM_12
 
-#define R_NOTIF 0x01
-#define B_NOTIF 0x02
-#define Y_NOTIF 0x03
-#define G_NOTIF 0x04
+Servo pwm0(PWM_0_PIN);
+Servo pwm1(PWM_1_PIN);
+Servo pwm2(PWM_2_PIN);
 
 #define HID_0 GPIO_NUM_16
 #define HID_1 GPIO_NUM_17
 
-Settingator& STR = Settingator::GetInstance();
-
-STR_Bool redLight(false, "RED_LIGHT");
-STR_Bool blueLight(false, "BLUE_LIGHT");
-STR_Bool yellowLight(false, "YELLOW_LIGHT");
-STR_Bool greenLight(false, "GREEN_LIGHT");
-
-bool redPressed = false;
-bool bluePressed = false;
-bool yellowPressed = false;
-bool greenPressed = false;
 bool hid1Pressed = false;
 bool hid0Pressed = false;
 
@@ -45,26 +30,8 @@ static void IRAM_ATTR buttonHandler(void* args)
 
 extern "C" void app_main(void)
 {
-	gpio_config_t ledButtonConfig = {
-		.pin_bit_mask =
-				(1ULL << R_LED) +
-				(1ULL << B_LED) +
-				(1ULL << Y_LED) +
-				(1ULL << G_LED),
-		.mode = GPIO_MODE_OUTPUT,
-		.pull_up_en = GPIO_PULLUP_DISABLE,
-		.pull_down_en = GPIO_PULLDOWN_ENABLE,
-		.intr_type = GPIO_INTR_DISABLE,
-	};
-
-	ESP_ERROR_CHECK(gpio_config(&ledButtonConfig));
-
 	gpio_config_t pushButtonConfig = {
 		.pin_bit_mask =
-				(1ULL << R_BUTTON) +
-				(1ULL << B_BUTTON) +
-				(1ULL << Y_BUTTON) +
-				(1ULL << G_BUTTON) +
 				(1ULL << HID_0) +
 				(1ULL << HID_1),
 		.mode = GPIO_MODE_INPUT,
@@ -77,35 +44,11 @@ extern "C" void app_main(void)
 
 	ESP_ERROR_CHECK(gpio_install_isr_service(0));
 
-	ESP_ERROR_CHECK(gpio_isr_handler_add(R_BUTTON, buttonHandler, &redPressed));
-	ESP_ERROR_CHECK(gpio_isr_handler_add(B_BUTTON, buttonHandler, &bluePressed));
-	ESP_ERROR_CHECK(gpio_isr_handler_add(Y_BUTTON, buttonHandler, &yellowPressed));
-	ESP_ERROR_CHECK(gpio_isr_handler_add(G_BUTTON, buttonHandler, &greenPressed));
 	ESP_ERROR_CHECK(gpio_isr_handler_add(HID_0, buttonHandler, &hid0Pressed));
 	ESP_ERROR_CHECK(gpio_isr_handler_add(HID_1, buttonHandler, &hid1Pressed));
 
-	ESP_ERROR_CHECK(gpio_set_intr_type(R_BUTTON, GPIO_INTR_POSEDGE));
-	ESP_ERROR_CHECK(gpio_set_intr_type(B_BUTTON, GPIO_INTR_POSEDGE));
-	ESP_ERROR_CHECK(gpio_set_intr_type(Y_BUTTON, GPIO_INTR_POSEDGE));
-	ESP_ERROR_CHECK(gpio_set_intr_type(G_BUTTON, GPIO_INTR_POSEDGE));
 	ESP_ERROR_CHECK(gpio_set_intr_type(HID_0, GPIO_INTR_POSEDGE));
 	ESP_ERROR_CHECK(gpio_set_intr_type(HID_1, GPIO_INTR_POSEDGE));
-
-	redLight.SetCallback(
-			[](){ ESP_ERROR_CHECK(gpio_set_level(R_LED, redLight)); }
-		);
-
-	blueLight.SetCallback(
-			[](){ ESP_ERROR_CHECK(gpio_set_level(B_LED, blueLight)); }
-		);
-
-	yellowLight.SetCallback(
-			[](){ ESP_ERROR_CHECK(gpio_set_level(Y_LED, yellowLight)); 
-		});
-
-	greenLight.SetCallback(
-			[](){ ESP_ERROR_CHECK(gpio_set_level(G_LED, greenLight)); }
-		);
 
 	for (auto i = 0; i < LS_0_LEN; i++)
 		Led::GetInstance().Strip0()[i] = RGB(255, 0, 0);
@@ -113,8 +56,13 @@ extern "C" void app_main(void)
 	for (auto i = 0; i < LS_1_LEN; i++)
 		Led::GetInstance().Strip1()[i] = RGB(0, 0, 255);
 
+
 	InitCores();
 	STR.begin();
+
+	pwm0.write(10);
+	pwm1.write(90.0f);
+	pwm2.write(170.0f);
 
 	ESP_LOGI("MAIN", "TEST");
 	while (true)
@@ -122,30 +70,6 @@ extern "C" void app_main(void)
 		Led::GetInstance().Show();
 		STR.Update();
 
-		if (redPressed)
-		{
-			redPressed = false;
-			STR.SendNotif(R_NOTIF);
-			ESP_LOGI("BUTTON", "RED");
-		}
-		if (bluePressed)
-		{
-			bluePressed = false;
-			STR.SendNotif(B_NOTIF);
-			ESP_LOGI("BUTTON", "BLUE");
-		}
-		if (yellowPressed)
-		{
-			yellowPressed = false;
-			STR.SendNotif(Y_NOTIF);
-			ESP_LOGI("BUTTON", "YELLOW");
-		}
-		if (greenPressed)
-		{
-			greenPressed = false;
-			STR.SendNotif(G_NOTIF);
-			ESP_LOGI("BUTTON", "GREEN");
-		}
 		if (hid0Pressed)
 		{
 			hid0Pressed = false;
