79bb817c55d4a0561495aa6966b9cfdfb846b733
cpm <cpm.cyril@gmail.com>
2026-09-12 19:52:42 +0200

    update

--- firmware/main/main.cpp
+++ firmware/main/main.cpp
@@ -9,9 +9,9 @@
 
 Settingator& STR = Settingator::GetInstance();
 
-#define PWM_0_PIN GPIO_NUM_27
+#define PWM_0_PIN GPIO_NUM_12
 #define PWM_1_PIN GPIO_NUM_14
-#define PWM_2_PIN GPIO_NUM_12
+#define PWM_2_PIN GPIO_NUM_27
 
 Servo pwm0(PWM_0_PIN, true);
 Servo pwm1(PWM_1_PIN, true);
@@ -21,6 +21,9 @@ STServoHandler STS(26, 25, 1000000, UART_NUM_2);
 STServo servo(0x01);
 
 STR_UInt16 pos(0, "POS");
+STR_Float pwm0Pos(0.0f, "0_POS");
+STR_Float pwm1Pos(0.0f, "1_POS");
+STR_Float pwm2Pos(0.0f, "2_POS");
 
 #define HID_0 GPIO_NUM_16
 #define HID_1 GPIO_NUM_17
@@ -57,27 +60,23 @@ extern "C" void app_main(void)
 	// ESP_ERROR_CHECK(gpio_set_intr_type(HID_0, GPIO_INTR_POSEDGE));
 	// ESP_ERROR_CHECK(gpio_set_intr_type(HID_1, GPIO_INTR_POSEDGE));
 
-	for (auto i = 0; i < LS_0_LEN; i++)
-		Led::GetInstance().Strip0()[i] = RGB(255, 0, 0);
-
-	for (auto i = 0; i < LS_1_LEN; i++)
-		Led::GetInstance().Strip1()[i] = RGB(0, 0, 255);
-
+	pwm0.begin();
+	pwm1.begin();
+	pwm2.begin();
 
 	InitCores();
 	STR.begin();
 
-	pwm0.write(5.0f);
-	pwm1.write(180.0f);
-	pwm2.write(180.0f);
-
 	servo.SetMaxAngle(32737);
 	servo.SetTargetPosition(8192);
+
+	pwm0Pos.SetCallback([]() {  pwm0.write(pwm0Pos); });
+	pwm1Pos.SetCallback([]() { pwm1.write(pwm1Pos); });
+	pwm2Pos.SetCallback([]() { pwm2.write(pwm2Pos); });
 
 	ESP_LOGI("MAIN", "TEST");
 	while (true)
 	{
-		Led::GetInstance().Show();
 		STR.Update();
 
 		if (hid0Pressed)
